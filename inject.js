ac415335ff3684d14c2f44fb207c86e553fa2417
Kai Marshland <kaimarshland@gmail.com>
2018-01-14 12:51:19 -0800

    Made it replace facebook emojis with the slack version

--- inject.js
+++ inject.js
@@ -1,5 +1,43 @@
 (function() {
 
-    alert(window.SLACK_EMOJI_URLS['ssi-41']);
+    let replacementInterval = null;
+
+    function replace() {
+        if (!$('._aok span').length) {
+
+            console.log('Waiting for messages to load...');
+            setTimeout(replace, 250);
+
+            if (replacementInterval) {
+                clearInterval(replacementInterval);
+            }
+
+            return;
+        }
+
+
+        $('._aok span').each(function () {
+            let content = $(this).html();
+
+            if (!/:[^:]+:/.test(content)) {
+                return;
+            }
+
+            content = content.replace(/:([^:]+):/g, function(match, emoji){
+                const emojiUrl = window.SLACK_EMOJI_URLS[emoji];
+                if (!emojiUrl) {
+                    return match;
+                }
+
+                return '<img src="' + emojiUrl + '" style="max-width: 30px">';
+            });
+
+            $(this).html(content);
+        });
+
+        replacementInterval = setInterval(replace, 1000);
+    }
+
+    replace();
 
 })();
